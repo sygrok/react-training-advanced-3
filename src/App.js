7d6c682ba57e5bd883e746225c48da8056b61495
Berkay Anduv <sygrok@hotmail.com>
2023-09-29 07:29:05 +0300

    CustomContextProvider

--- src/App.js
+++ src/App.js
@@ -1,50 +1,74 @@
-import React, { useEffect, useState } from "react";
-
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import { useContext } from "react";
 import AuthContext from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const storedUserLoggedIn = localStorage.getItem("isLoggedIn");
-
-    if (storedUserLoggedIn === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false);
-  };
+  const ctx = useContext(AuthContext);
+  //We have added context to index.js to reach globally
 
   return (
-    // <React.Fragment> Since we use Auhcontext with 'Provider' we don't need react fragment anymore
-    // With the 'Provider' context can wrap jsx code and all of the components child can accesss
-    // We can pass functions to make context dynamic
-
-    <AuthContext.Provider
-      value={{
-        isLoggedIn: isLoggedIn,
-        onLogout: logoutHandler, //function
-        onLogin: loginHandler, //function
-      }}
-    >
+    <>
       <MainHeader />
       <main>
-        {!isLoggedIn && <Login />}
-        {isLoggedIn && <Home />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-    </AuthContext.Provider>
+    </>
   );
 }
 
 export default App;
+
+// <React.Fragment> Since we use Auhcontext with 'Provider' we don't need react fragment anymore
+// With the 'Provider' context can wrap jsx code and all of the components child can accesss
+// We can pass functions to make context dynamic
+
+////////////////////////////////////////
+
+//First use of useContext without custom context
+
+// function App() {
+// const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+// useEffect(() => {
+//   const storedUserLoggedIn = localStorage.getItem("isLoggedIn");
+
+//   if (storedUserLoggedIn === "1") {
+//     setIsLoggedIn(true);
+//   }
+// }, []);
+
+// const loginHandler = (email, password) => {
+//   localStorage.setItem("isLoggedIn", "1");
+//   setIsLoggedIn(true);
+// };
+
+// const logoutHandler = () => {
+//   localStorage.removeItem("isLoggedIn");
+//   setIsLoggedIn(false);
+// };
+
+// return (
+// <React.Fragment> Since we use Auhcontext with 'Provider' we don't need react fragment anymore
+// With the 'Provider' context can wrap jsx code and all of the components child can accesss
+// We can pass functions to make context dynamic
+
+//     <AuthContext.Provider
+//       value={{
+//         isLoggedIn: isLoggedIn,
+//         onLogout: logoutHandler, //function
+//         onLogin: loginHandler, //function
+//       }}
+//     >
+//       <MainHeader />
+//       <main>
+//         {!isLoggedIn && <Login />}
+//         {isLoggedIn && <Home />}
+//       </main>
+//     </AuthContext.Provider>
+//   );
+// }
+
+// export default App;
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,10 +1,46 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const AuthContext = createContext({
   //we can leave this blank but if we add the corresponding values, auto compilation will be enabled.
   isLoggedIn: false,
   onLogout: () => {},
-  onLogin: () => {},
+  onLogin: (email, password) => {},
 });
+
+//Custom Context Provider Component
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedIn === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
