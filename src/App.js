c61630998cb60fddebdf92995c238e301b038968
Berkay Anduv <sygrok@hotmail.com>
2023-09-29 06:00:09 +0300

    CreateContext&UseContext

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -26,13 +27,16 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    // <React.Fragment> Since we use Auhcontext with 'Provider' we don't need react fragment anymore
+    // With the 'Provider' context can wrap jsx code and all of the components child can access
+
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
